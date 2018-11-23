65b3ad3da50ec0e49ae0e109ab57b4572f255025
Scott Gibb CESM2016 <scott.gibb.2016@uni.strath.ac.uk>
2018-11-23 18:05:47 +0000

    Upload New File

---
+++ src/Model.java
@@ -0,0 +1,64 @@
+import java.util.Observable;
+
+public class Model extends Observable implements IModel {
+
+
+    private int count;
+
+    /**
+     * Requires: start != null
+     * modifies: this
+     * effects: sets the initial value of count
+     *
+     */
+    public Model(int start){
+        count= start;
+    }
+
+    /**
+     * modifies: this
+     * effects: increments count and notifys the observers that things have chnaged
+     */
+    @Override
+    public void inc() {
+        count++;
+        System.out.println("Count is"+ count);
+        setChanged();//set the observaable object ie the model to changed
+        notifyObservers();//notify all the observers
+    }
+
+    /**
+     * Modifies: this
+     * effetcs: decrements the count and notifies hte observs that things have changed
+     */
+    @Override
+    public void dec() {
+        count--;
+        System.out.println("Count is"+ count);
+        setChanged();//set the observaable object ie the model to changed
+        notifyObservers();//notify all the observers
+    }
+
+    /**
+     *
+     * effects: returns the current count
+     */
+    @Override
+    public int getCount() {
+        return count;
+
+    }
+
+    /**
+     * requires: count != null
+     * Modifies: this
+     * effects: sets the current count to count and notifys the observers that its changed
+     */
+    @Override
+    public void setCount(int count) {
+        this.count = count;
+        System.out.println("Count is"+ count);
+        setChanged();
+        notifyObservers();
+    }
+}
